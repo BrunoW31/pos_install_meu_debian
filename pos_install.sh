969471245742863ce7757fa1696d1b7e6b67f483
BrunoSimonato <brunoosimonato@proton.me>
2025-06-30 20:38:17 -0400

    Update pos_install.sh

--- pos_install.sh
+++ pos_install.sh
@@ -7,15 +7,15 @@
 ########################################
 
 #distro = /etc/os-release | grep "PRETTY_NAME" | cut -d "\"" -f 2
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y;
 
 # instalar google chrome
 cd ~/Downloads
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+sudo apt install ./google-chrome-stable_current_amd64.deb -y;
 
 # instalar burpsuite
-wget https://portswigger.net/burp/releases/download?product=pro&version=2025.5.6&type=Linux
+wget https://portswigger.net/burp/releases/download?product=pro&version=2025.5.6&type=Linux;
 cd ~/Downloads
 chmod +x burpsuite_pro_linux_v2025_5_6.sh
 ./burpsuite_pro_linux_v2025_5_6.sh
@@ -26,16 +26,16 @@ sudo apt install flatpak -y;
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # instalar docker
-sudo apt install docker docker-compose docker.io
+sudo apt install docker docker-compose docker.io;
 
 # instalar kvm
-sudo apt install qemu-system libvirt-daemon-system virt-manager
+sudo apt install qemu-system libvirt-daemon-system virt-manager;
 
 # instalar servidor web + mysql
-sudo apt install apache2 mariadb-server php phpmyadmin
+sudo apt install apache2 mariadb-server php phpmyadmin;
 
 # ferramentas de testes de rede
-sudo apt install sqlmap nmap netdiscover hydra john hashcat aircrack-ng wifite gobuster dirb ffuf kleopatra 
+sudo apt install sqlmap nmap netdiscover hydra john hashcat aircrack-ng wifite gobuster dirb ffuf kleopatra ;
 
 
 
